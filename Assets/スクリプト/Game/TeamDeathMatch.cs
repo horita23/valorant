b063239cc6af68b7d705c73b5a0fba625fd00182
horita23 <it222222@torident.ac.jp>
2024-11-14 12:25:44 +0900

    バグ修正

--- Assets/スクリプト/Game/TeamDeathMatch.cs
+++ Assets/スクリプト/Game/TeamDeathMatch.cs
@@ -207,6 +207,7 @@ public class TeamDeathMatch : MonoBehaviour
                     {
                         if (PhotonNetwork.InRoom)
                         {
+                            PhotonNetwork.LocalPlayer.CustomProperties.Clear();
                             PhotonNetwork.LeaveRoom();
                         }
                         PhotonNetwork.Disconnect();
